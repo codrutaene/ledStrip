3aa921be9bd587b374cccaaa1018085dfbd0b9ea
AD001\z002txtf <codruta.ene@siemens.com>
2019-03-27 10:43:44 +0200

    added minuteClock

--- rtc.ino
+++ rtc.ino
@@ -126,8 +126,9 @@ int  txtMCINCI[] = {24,25,26,27,28,-1};
 int  txtMZECE[] = {29,30,31,32,-1};
 int  txtMSFERT[] = {44,45,6,7,8,9,10,-1};
 int  txtMDOUAZECI[] = {33,34,35,36,37,38,39,40,-1};
-int  txtMDOUAZECSICINCI[] = {33,34,35,36,37,38,39,40,42,43,24,25,26,27,28,-1};
+int  txtMSICINCI[] = {42,43,24,25,26,27,28,-1};
 int  txtMJUMATATE[] = {13,14,15,16,17,18,19,20,-1};
+int  txtMMINUTE[] = {0,1,2,3,4,5,-1};
 
 //****************COLORS***********//
 uint32_t Red = CRGB::Red; 
@@ -190,7 +191,7 @@ void setup()
 //    displayTime(hsv_hue, hsv_sat, hsv_val);
     paintWord(txtESTEORA, NELEMS(txtESTEORA), White); 
     HourClock(); //void HourClock
-//    MinuteClock(); // void MinuteClock
+    MinuteClock(); // void MinuteClock
   }
 
 }
@@ -200,7 +201,7 @@ void loop()
 //  gModes[gCurrentModeNumber]();
 //  displayTime(hsv_hue, hsv_sat, hsv_val);
     HourClock(); //void HourClock
-//    MinuteClock(); // void MinuteClock
+    MinuteClock(); // void MinuteClock
   
   if (digitalRead(min) == 0){    
     //call increaseMin
@@ -272,35 +273,23 @@ void increaseMin() {
   { 
     t = rtc.getTime();  
     int minupg = t.min;
-  
+    int hourpg = t.hour;
     if (minupg==59)
     {
-      minupg=0;
+      minupg = 0;
+      hourpg = hourpg + 1;
     }
     else
     {
-      minupg=minupg+1;
+      minupg = minupg + 1;
     } 
     
-    rtc.setTime(t.hour, minupg, 0);
+    rtc.setTime(hourpg, minupg, 0);
     Serial.println("min button pushed! ");
     Serial.println(rtc.getTimeStr());
     delay(200);
   }
 }
-
-//int  txtUNU[] = {120,119,118,-1};
-//int  txtDOUA[] = {117,116,115,114,-1};
-//int  txtTREI[] = {113,112,111,110,-1};
-//int  txtPATRU[] = {105,106,107,108,109,-1};
-//int  txtCINCI[] = {99,100,101,102,103,-1};
-//int  txtSASE[] = {62,63,64,65,-1};
-//int  txtSAPTE[] = {77,78,79,80,81,-1};
-//int  txtOPT[] = {55,56,57,-1};
-//int  txtNOUA[] = {82,83,84,85,-1};
-//int  txtZECE[] = {58,59,60,61,-1};
-//int  txtUNSPREZECE[] = {66,67,68,69,70,71,72,73,74,75,-1};
-//int  txtDOISPREZECE[] = {88,89,90,91,92,93,94,95,96,97,98,-1};
 
 void HourClock()
 {
@@ -308,7 +297,7 @@ void HourClock()
   int timeMin = t.min;
   int modMIn = timeMin % 5;
   int ora = t.hour;
-  if (timeMin >= 40){
+  if (timeMin >= 35){
     ora = ora + 1;  
   }
   
@@ -317,88 +306,254 @@ void HourClock()
     case 12: 
     case 24:
         paintWord(txtUNSPREZECE, NELEMS(txtUNSPREZECE), Off);
-        FastLED.show();    
         paintWord(txtDOISPREZECE, NELEMS(txtDOISPREZECE), White);
         FastLED.show();                 
         break;
     case 1:
     case 13:
         paintWord(txtDOISPREZECE, NELEMS(txtDOISPREZECE), Off);
-        FastLED.show();    
         paintWord(txtUNU, NELEMS(txtUNU), White);
         FastLED.show();
         break;
     case 2:
     case 14:
         paintWord(txtUNU, NELEMS(txtUNU), Off);
-        FastLED.show();    
         paintWord(txtDOUA, NELEMS(txtDOUA), White);
         FastLED.show();        
         break;
     case 3:
     case 15:        
         paintWord(txtDOUA, NELEMS(txtDOUA), Off);
-        FastLED.show();    
         paintWord(txtTREI, NELEMS(txtTREI), White);
         FastLED.show();
         break;
     case 4:
     case 16:       
         paintWord(txtTREI, NELEMS(txtTREI), Off);
-        FastLED.show();        
         paintWord(txtPATRU, NELEMS(txtPATRU), White);
         FastLED.show();
         break;
     case 5:
     case 17:       
         paintWord(txtPATRU, NELEMS(txtPATRU), Off);
-        FastLED.show();    
         paintWord(txtCINCI, NELEMS(txtCINCI), White);
         FastLED.show();
         break;
     case 6:
     case 18:        
         paintWord(txtCINCI, NELEMS(txtCINCI), Off);
-        FastLED.show();
         paintWord(txtSASE, NELEMS(txtSASE), White);
         FastLED.show();
         break;
     case 7:
     case 19:       
         paintWord(txtSASE, NELEMS(txtSASE), Off);
-        FastLED.show();
         paintWord(txtSAPTE, NELEMS(txtSAPTE), White);
         FastLED.show();
         break;
     case 8:
     case 20:        
         paintWord(txtSAPTE, NELEMS(txtSAPTE), Off);
-        FastLED.show();
         paintWord(txtOPT, NELEMS(txtOPT), White);
         FastLED.show();
         break;
     case 9:
     case 21:
         paintWord(txtOPT, NELEMS(txtOPT), Off);
-        FastLED.show();
         paintWord(txtNOUA, NELEMS(txtNOUA), White);
         FastLED.show();
         break;
     case 10:
     case 22:
         paintWord(txtNOUA, NELEMS(txtNOUA), Off);
-        FastLED.show();
         paintWord(txtZECE, NELEMS(txtZECE), White);
         FastLED.show();
         break;
     case 11:
     case 23:
         paintWord(txtZECE, NELEMS(txtZECE), Off);
-        FastLED.show();
         paintWord(txtUNSPREZECE, NELEMS(txtUNSPREZECE), White);
         FastLED.show();
         break;
     }
+}
+
+////********Minutes*********//
+//int  txtSI[] = {49,50,-1};
+//int  txtFARA[] = {51,52,53,54,-1};
+//int  txtFIX[] = {46,47,48,-1};
+//int  txtMCINCI[] = {24,25,26,27,28,-1};
+//int  txtMZECE[] = {29,30,31,32,-1};
+//int  txtMSFERT[] = {44,45,6,7,8,9,10,-1};
+//int  txtMDOUAZECI[] = {33,34,35,36,37,38,39,40,-1};
+//int  txtMSICINCI[] = {42,43,24,25,26,27,28,-1};
+//int  txtMJUMATATE[] = {13,14,15,16,17,18,19,20,-1};
+
+void MinuteClock() {
+  t = rtc.getTime();  
+  int timeMin = t.min;
+  int modMIn = timeMin % 5;
+//  int ora = t.hour;
+//  if (timeMin >= 40){
+//    ora = ora + 1;  
+//  }
+//  if (min < 5) {
+//        paintWord(txtZECE, NELEMS(txtZECE), Off);
+//        paintWord(txtUNSPREZECE, NELEMS(txtUNSPREZECE), White);
+//        FastLED.show();
+//  }
+//  else if (min >=5 && min < 35){
+//    
+//  }else
+  switch (timeMin) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMCINCI, NELEMS(txtMCINCI), Off);
+      paintWord(txtFIX, NELEMS(txtFIX), White);  
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), Off);    
+      FastLED.show();
+      break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMCINCI, NELEMS(txtMCINCI), White);
+      paintWord(txtFIX, NELEMS(txtFIX), Off); 
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);                 
+      FastLED.show();
+      break;
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMCINCI, NELEMS(txtMCINCI), Off);
+      paintWord(txtMZECE, NELEMS(txtMZECE), White);      
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);            
+      FastLED.show();
+      break;
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMZECE, NELEMS(txtMZECE), Off);
+      paintWord(txtMSFERT, NELEMS(txtMSFERT), White);    
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), Off);  
+      FastLED.show();
+      break;
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMSFERT, NELEMS(txtMSFERT), Off);
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), White);  
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);                
+      FastLED.show();
+      break;
+    case 25:
+    case 26:
+    case 27:
+    case 28:
+    case 29:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), White);
+      paintWord(txtMSICINCI, NELEMS(txtMSICINCI), White); 
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);                 
+      FastLED.show();
+      break;
+    case 30:
+    case 31:
+    case 32:
+    case 33:
+    case 34:
+      paintWord(txtSI, NELEMS(txtSI), White);
+      paintWord(txtFARA, NELEMS(txtFARA), Off);
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), Off);
+      paintWord(txtMSICINCI, NELEMS(txtMSICINCI), Off);    
+      paintWord(txtMJUMATATE, NELEMS(txtMJUMATATE), White); 
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), Off);         
+      FastLED.show();
+      break;
+    case 35:
+    case 36:
+    case 37:
+    case 38:
+    case 39:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), White);
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), White);
+      paintWord(txtMSICINCI, NELEMS(txtMSICINCI), White);  
+      paintWord(txtMJUMATATE, NELEMS(txtMJUMATATE), Off);   
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);                   
+      FastLED.show();
+      break;
+    case 40:
+    case 41:
+    case 42:
+    case 43:
+    case 44:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), White);
+      paintWord(txtMSICINCI, NELEMS(txtMSICINCI), Off);  
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), White); 
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);                     
+      FastLED.show();
+      break;
+    case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 49:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), White);
+      paintWord(txtMDOUAZECI, NELEMS(txtMDOUAZECI), Off);  
+      paintWord(txtMSFERT, NELEMS(txtMSFERT), White);   
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), Off);            
+      FastLED.show();
+      break;
+    case 50:
+    case 51:
+    case 52:
+    case 53:
+    case 54:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), White);
+      paintWord(txtMSFERT, NELEMS(txtMSFERT), Off);  
+      paintWord(txtMZECE, NELEMS(txtMZECE), White); 
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);         
+      FastLED.show();
+      break;
+    case 55:
+    case 56:
+    case 57:
+    case 58:
+    case 59:
+      paintWord(txtSI, NELEMS(txtSI), Off);
+      paintWord(txtFARA, NELEMS(txtFARA), White);
+      paintWord(txtMZECE, NELEMS(txtMZECE), Off);  
+      paintWord(txtMCINCI, NELEMS(txtMCINCI), White);
+      paintWord(txtMMINUTE, NELEMS(txtMMINUTE), White);         
+      FastLED.show();
+      break;    
+  }
+
 }
 void paintWord(int arrWord[], int sizeArr, uint32_t intColor){
     for(int i = 0; i < sizeArr; i++){
